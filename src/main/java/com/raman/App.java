de6014c0041daaa75b04c74c978d642211f6da78
abira <abiradey92@gmail.com>
2021-07-19 17:45:55 +0000

    app is updated

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Maven Job is running on Build Server! By Abira-Testing-update" );
+        System.out.println( "Maven Job is running on Build Server! By Abira-Testing-update-with-cicd" );
     }
 }
