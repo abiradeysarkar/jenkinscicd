90681280b76faf313b0c8bc3d95d261bafefd009
abiradeysarkar <84263899+abiradeysarkar@users.noreply.github.com>
2021-07-19 22:49:56 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Maven Job is running on Build Server! By Abira-Testing" );
+        System.out.println( "Maven Job is running on Build Server! By Abira-Testing-update" );
     }
 }
